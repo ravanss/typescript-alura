c1190e3c8e4d65548812596e35cdff2a79ffcaf2
ravanss <psaulo7@gmail.com>
2023-03-13 16:23:46 -0300

    subindo repositorio no github inicio do projeto

---
+++ dist/js/app.js
@@ -0,0 +1,6 @@
+import { Negociacao } from './models/negociacao.js';
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao); //todos os valores da class
+console.log(negociacao.data);
+console.log(negociacao.valor);
+console.log(negociacao.volume); //operacao matematica
