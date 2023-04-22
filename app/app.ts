ad850a593bcf618e088a9b1920ab212790a0b7fd
ravanss <psaulo7@gmail.com>
2023-04-22 14:58:23 -0300

    atualizando projeto

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,16 @@
-import { Negociacao } from './models/negociacao.js';
+/*import { Negociacao } from './models/negociacao.js';
 
 const negociacao = new Negociacao (new Date(), 10 , 100);
 console.log(negociacao); //todos os valores da class
 console.log(negociacao.data);
 console.log(negociacao.valor);
-console.log(negociacao.volume); //operacao matematica
+console.log(negociacao.volume); //operacao matematica*/
+
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,14 @@
-import { Negociacao } from './models/negociacao.js';
-const negociacao = new Negociacao(new Date(), 10, 100);
+/*import { Negociacao } from './models/negociacao.js';
+
+const negociacao = new Negociacao (new Date(), 10 , 100);
 console.log(negociacao); //todos os valores da class
 console.log(negociacao.data);
 console.log(negociacao.valor);
-console.log(negociacao.volume); //operacao matematica
+console.log(negociacao.volume); //operacao matematica*/
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
