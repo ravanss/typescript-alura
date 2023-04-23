540263c2d7b545d3c8c2cde00d2ed7ee9669eca6
ravanss <psaulo7@gmail.com>
2023-04-23 18:24:45 -0300

    versão final do projeto

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,28 +1,18 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor (
+        private _data: Date,
+        public readonly quantidade: number, 
+        public readonly valor: number
+    ) {}
+
+    get volume(): Number {
+        return this.quantidade * this.valor;
     }
 
     get data(): Date{
+        const data = new Date(this._data.getTime());
         return this._data;
-    }
-
-    get quantidade(): Number{
-        return this._quantidade;
-    }
-
-    get valor(): Number{
-        return this._valor;
-    }
-
-    get volume(): Number {
-        return this._quantidade * this._valor;
     }
     
 }
